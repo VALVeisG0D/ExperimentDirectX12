5fe757fcf681e868b6b072bf0b8d7f2b2f6a8da0
Vichaya Chantamoke <vichaya_chantamoke@hotmail.com>
2018-10-29 02:08:56 -0700

    Begin expanding to the 2nd dimension

--- ExperimentDirectX12/Content/Sample3DSceneRenderer.cpp
+++ ExperimentDirectX12/Content/Sample3DSceneRenderer.cpp
@@ -309,9 +309,9 @@ void Sample3DSceneRenderer::Update(DX::StepTimer const& timer)
 			//Rotate(m_angle);
 			VertexPositionColor instanceData[] =
 			{
-				{ XMFLOAT3(field.fieldIndexToCoordinate(0), 0.0f, 0.0f), XMFLOAT3(1.0f, 1.0f, 1.0f) },
-			{ XMFLOAT3(field.fieldIndexToCoordinate(1), 0.0f, 0.0f), XMFLOAT3(1.0f, 1.0f, 1.0f) },
-			{ XMFLOAT3(field.fieldIndexToCoordinate(2), 0.0f, 0.0f), XMFLOAT3(0.0f, 0.0f, 0.0f) }
+				{ XMFLOAT3(field.xFieldIndexToCoordinate(0), field.yCoordinateToFieldIndex(0), 0.0f), XMFLOAT3(1.0f, 1.0f, 1.0f) },
+			{ XMFLOAT3(field.xFieldIndexToCoordinate(1), field.yCoordinateToFieldIndex(1), 0.0f), XMFLOAT3(1.0f, 1.0f, 1.0f) },
+			{ XMFLOAT3(field.xFieldIndexToCoordinate(2), field.yCoordinateToFieldIndex(2), 0.0f), XMFLOAT3(0.0f, 0.0f, 0.0f) }
 			};
 
 			UpdateVertexBuffer(sizeof(instanceData), instanceData, instanceBufferUpload);
--- ExperimentDirectX12/Field.h
+++ ExperimentDirectX12/Field.h
@@ -1,14 +1,17 @@
 // Speed of light exists because the computer simulating our reality is not powerful enough to perform computation beyond that limit.
 #pragma once
+#define DEFAULT_DIMENSION 256
+
 class Field
 {
-	int (*field);
-	const size_t DEFAULT_DIMENSION = 256;
+	int (*field)[DEFAULT_DIMENSION];
 
 	struct Particle
 	{
 		int xCoordinate;
 		int xInertia;
+		int yCoordinate;
+		int yInertia;
 	};
 
 	const size_t DEFAULT_NUMBER_OF_PARTICLES = 3;
@@ -18,28 +21,36 @@ public:
 	Field();
 	~Field();
 
-	void AddParticle(int);
-	void RemoveParticle(int);
+	void AddParticle(int, int);
+	void RemoveParticle(int, int);
 	void UpdateParticlePosition();
-	float fieldIndexToCoordinate(int);
-	size_t coordinateToFieldIndex(int);
+	float xFieldIndexToCoordinate(int);
+	float yFieldIndexToCoordinate(int);
+	size_t xCoordinateToFieldIndex(int);
+	size_t yCoordinateToFieldIndex(int);
 };
 
 Field::Field()
 {
 	// Create field array and particles
-	field = new int[DEFAULT_DIMENSION]();
+	field = new int[DEFAULT_DIMENSION][DEFAULT_DIMENSION]();
 	particleList = new Particle[DEFAULT_NUMBER_OF_PARTICLES]();
 
 	// Create barrier along edge of field so that particles don't
 	// go out of bound
-	field[0] = 1;
-	field[DEFAULT_DIMENSION - 1] = 1;
+
+	//	Top and bottom edge
+	for (int x = 0; x < DEFAULT_DIMENSION; ++x)
+		field[0][x] = field[DEFAULT_DIMENSION - 1][x] = 1;
+
+	//	Left and right edge
+	for (int y = 1; y < DEFAULT_DIMENSION - 1; ++y)
+		field[y][0] = field[y][DEFAULT_DIMENSION - 1] = 1;
 	
 	// Add particles
-	AddParticle(particleList[0].xCoordinate = coordinateToFieldIndex(-2));
-	AddParticle(particleList[1].xCoordinate = coordinateToFieldIndex(0));
-	AddParticle(particleList[2].xCoordinate = coordinateToFieldIndex(2));
+	AddParticle(particleList[0].yCoordinate = yCoordinateToFieldIndex(0), particleList[0].xCoordinate = xCoordinateToFieldIndex(-2));
+	AddParticle(particleList[1].yCoordinate = yCoordinateToFieldIndex(0), particleList[1].xCoordinate = xCoordinateToFieldIndex(0));
+	AddParticle(particleList[2].yCoordinate = yCoordinateToFieldIndex(0), particleList[2].xCoordinate = xCoordinateToFieldIndex(2));
 }
 
 Field::~Field()
@@ -48,47 +59,76 @@ Field::~Field()
 	delete[] particleList;
 }
 
-inline void Field::AddParticle(int coordinate)
+inline void Field::AddParticle(int yCoordinate, int xCoordinate)
 {
 	//convert coordinate value to index value
 	//what the hell value to give to that field coordinate
-	field[coordinate - 1] += 1;
-	field[coordinate] += 64;
-	field[coordinate + 1] += 1;
+	field[yCoordinate + 1][xCoordinate - 1] += 1;
+	field[yCoordinate + 1][xCoordinate] += 1;
+	field[yCoordinate + 1][xCoordinate + 1] += 1;
+
+	field[yCoordinate][xCoordinate - 1] += 1;
+	field[yCoordinate][xCoordinate] += 64;
+	field[yCoordinate][xCoordinate + 1] += 1;
+
+	field[yCoordinate - 1][xCoordinate - 1] += 1;
+	field[yCoordinate - 1][xCoordinate] += 1;
+	field[yCoordinate - 1][xCoordinate + 1] += 1;
 }
 
-inline void Field::RemoveParticle(int coordinate)
+inline void Field::RemoveParticle(int yCoordinate, int xCoordinate)
 {
-	field[coordinate - 1] -= 1;
-	field[coordinate] -= 64;
-	field[coordinate + 1] -= 1;
+	field[yCoordinate + 1][xCoordinate - 1] -= 1;
+	field[yCoordinate + 1][xCoordinate] -= 1;
+	field[yCoordinate + 1][xCoordinate + 1] -= 1;
+
+	field[yCoordinate][xCoordinate - 1] -= 1;
+	field[yCoordinate][xCoordinate] -= 64;
+	field[yCoordinate][xCoordinate + 1] -= 1;
+
+	field[yCoordinate - 1][xCoordinate - 1] -= 1;
+	field[yCoordinate - 1][xCoordinate] -= 1;
+	field[yCoordinate - 1][xCoordinate + 1] -= 1;
 }
 
 inline void Field::UpdateParticlePosition()
 {
 	// 3 steps: calculate inertia, delete old position, add new position
 	for (size_t i = 0; i < DEFAULT_NUMBER_OF_PARTICLES; ++i)
+	{
+		//inertia calculations becomes more complicated for 2 dimensionals
 		particleList[i].xInertia += field[particleList[i].xCoordinate - 1] - field[particleList[i].xCoordinate + 1];
+	}
 
 	// Move the particle by removing from its old position and placing it at the new one
 	// What if they are at the same position? Then try not to put particles in the same position
 	for (size_t i = 0; i < DEFAULT_NUMBER_OF_PARTICLES; ++i)
 	{
-		RemoveParticle(particleList[i].xCoordinate);
-		AddParticle(particleList[i].xCoordinate += particleList[i].xInertia);
+		RemoveParticle(particleList[i].yCoordinate, particleList[i].xCoordinate);
+		AddParticle(particleList[i].yCoordinate += particleList[i].yInertia, particleList[i].xCoordinate += particleList[i].xInertia);
 	}
 }
 
 //	Convert from index to coordinate
-inline float Field::fieldIndexToCoordinate(int particleListNumber)
+inline float Field::xFieldIndexToCoordinate(int particleListNumber)
 {
 	return (particleList[particleListNumber].xCoordinate - ((float)DEFAULT_DIMENSION / 2.0f)) * 0.01f;
 }
 
-//	Convert from coordinate to index
-inline size_t Field::coordinateToFieldIndex(int coordinate)
+inline float Field::yFieldIndexToCoordinate(int particleListNumber)
 {
-	return size_t(coordinate) + (DEFAULT_DIMENSION / 2);
+	return (particleList[particleListNumber].yCoordinate - ((float)DEFAULT_DIMENSION / 2.0f)) * 0.01f;
+}
+
+//	Convert from coordinate to index
+inline size_t Field::xCoordinateToFieldIndex(int xCoordinate)
+{
+	return size_t(xCoordinate) + (DEFAULT_DIMENSION / 2);
+}
+
+inline size_t Field::yCoordinateToFieldIndex(int yCoordinate)
+{
+	return size_t(yCoordinate) + (DEFAULT_DIMENSION / 2);
 }
 
 
