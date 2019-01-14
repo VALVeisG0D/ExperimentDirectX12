7adca21d140d5f3bac2810ee24d0ec41a013455b
Vichaya Chantamoke <vichaya_chantamoke@hotmail.com>
2019-01-14 06:06:56 -0800

    Working on 3D boundary

--- ExperimentDirectX12/Field.h
+++ ExperimentDirectX12/Field.h
@@ -47,14 +47,16 @@ Field::Field()
 	//	Top and bottom plane
 	for (int x = 0; x < DEFAULT_DIMENSION; ++x)
 		for (int z = 0; z < DEFAULT_DIMENSION; ++z)
-			field[z][DEFAULT_DIMENSION][x] = 
+			field[DEFAULT_DIMENSION - 1][x][z] = field[0][x][z] = 1;
 
-	for (int x = 0; x < DEFAULT_DIMENSION; ++x)
-		field[0][x] = field[DEFAULT_DIMENSION - 1][x] = 1;
+	//	Left and Right plane
+	
 
-	//	Left and right edge
-	for (int y = 1; y < DEFAULT_DIMENSION - 1; ++y)
-		field[y][0] = field[y][DEFAULT_DIMENSION - 1] = 1;
+	//for (int x = 0; x < DEFAULT_DIMENSION; ++x)
+	//	field[0][x] = field[DEFAULT_DIMENSION - 1][x] = 1;
+
+	//for (int y = 1; y < DEFAULT_DIMENSION - 1; ++y)
+	//	field[y][0] = field[y][DEFAULT_DIMENSION - 1] = 1;
 	
 	// Add particles
 	AddParticle(particleList[0].yCoordinate = coordinateToFieldIndex(0), 
