2292b0ca62a9d9d2024f7c6537f8bc352054c572
VALVeisG0D <vichaya_chantamoke@hotmail.com>
2019-02-20 10:17:08 -0800

    Begin implementing camera movement and control logic

---
+++ ExperimentDirectX12/MoveLookController.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <DirectXMath.h>
+
+using namespace Windows::UI::Core;
+using namespace Windows::System;
+using namespace Windows::Foundation;
+using namespace Windows::Devices::Input;
+
+constexpr auto ROTATION_GAIN = 0.004f;		// Sensitivity adjustment for the look controller
+constexpr auto MOVEMENT_GAIN = 0.1f;		// Sensitivity adjustment for the move controller
+
+class MoveLookController
+{
+	// Properties of the controller object
+public:
+
+};
